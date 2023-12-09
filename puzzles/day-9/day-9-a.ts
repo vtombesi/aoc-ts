36f0fdc706a11f0c6df50c49b5187c632162b102
Valentino Tombesi <valentino.tombesi@gmail.com>
2023-12-09 15:24:20 +0100

    day 9: done

--- puzzles/day-9/day-9-a.ts
+++ puzzles/day-9/day-9-a.ts
@@ -1,9 +1,35 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+function extrapolate(arr: string | any[]) {
+  const difference = [];
+  if (arr.length === 0) {
+    return 0;
+  }
+  for (let i = 1; i < arr.length; i++) {
+    i = parseInt(i.toString());
+    difference.push(arr[i] - arr[i - 1]);
+  }
+
+  return arr[arr.length - 1] + extrapolate(difference);
+}
+
 export async function day9a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = await readData(dataPath, false);
+  const lines = data.split('\n').map((line: string) => {
+    return line.split(' ').map((n: string) => {
+      return parseInt(n);
+    });
+  });
+  const post = lines.map((line: any) => {
+    return extrapolate(line);
+  });
+  return post.reduce((acc: any, n: number) => {
+    if (isNaN(n)) {
+      return acc;
+    }
+    return acc + n;
+  }, 0);
 }
 
 const answer = await day9a();
